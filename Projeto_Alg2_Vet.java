9b78c06e1e47982b19902deffeed5765add9ab8b
Pedro Ibanez <pedroibanez913@gmail.com>
2024-09-06 17:38:58 -0300

    minimo

--- Projeto_Alg2_Vet.java
+++ Projeto_Alg2_Vet.java
@@ -21,12 +21,14 @@ import java.util.Scanner;
 public class Projeto_Alg2_Vet {
     public static Scanner leitor = new Scanner(System.in);
     public static void main(String[] args) {
-
+        int[] vetor = new int[1000];
         int comando;
 
         do{
 
-        System.out.printf("Escolha uma opção:\n" +
+        System.out.printf(
+        "Bem vindo ao sistema de operação com vetores\n"+
+        "Escolha uma opção:\n" +
         "1. Inicializar o vetor com números aleatórios\n" +
         "2. Imprimir o vetor\n" +
         "3. Verificar se um determinado número está contido no vetor\n" +
@@ -35,18 +37,17 @@ public class Projeto_Alg2_Vet {
         "6. Calcular o percentual dos números ímpares armazenados no vetor\n" +
         "7. Calcular a média centralizada dos números armazenados no vetor\n" +
         "8. Verificar se dois números somam um valor específico\n" +
-        "9. Voltar\n" +
-        "10. Sair\n");
+        "0. Sair\n");
 
         
             comando = leitor.nextInt();
 
             switch (comando) {
                 case 1:
-                    function1(null);
+                    function1(vetor);
                     break;
                 case 2:
-                    function2(null);
+                    function2(vetor);
                     break;
                 case 3:
                     
@@ -78,22 +79,21 @@ public class Projeto_Alg2_Vet {
                     break;
             }
             
-         }while(comando!=10);
+         }while(comando!=0);
     }
     
 
 
 
-    public static int[] function1 (int[] n ){
+    public static void function1 (int[] vetor ){
 
-        Random r = new Random();
+        Random random = new Random();
     
-        for(int i = 0; i < n.length; i++){
+        for(int i = 0; i < vetor.length; i++){
     
-            n[i] = r.nextInt(1000);
+            vetor[i] = random.nextInt(1000);
     
-        }
-        return n;
+         }
         }
 
        
