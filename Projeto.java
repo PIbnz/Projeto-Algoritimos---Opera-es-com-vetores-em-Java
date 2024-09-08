4b6ca296391e122a72e7b714a3f2387d3238ad1f
Pedro Ibanez <pedroibanez913@gmail.com>
2024-09-08 19:30:10 -0300

    documentação

--- Projeto.java
+++ Projeto.java
@@ -23,12 +23,12 @@ public class Projeto {
         Random random = new Random();
         Scanner leitor = new Scanner(System.in);
 
-        int comando = 0;
-        int n = 0;
-        int m = 0;
+        int comando = 0; /* guarda a escolha do usuário, "1 obrigatório", 2,3,5 e etc... */
+        int n = 0; /* guarda o tamanho do vetor */
+        int m = 0; /* guarda o maior valor dentro do vetor */
         
 
-        System.out.printf(
+        System.out.printf(    /*Apresenta no terminal o menu de opções */
         "**************************************************\n"+
         "***Bem vindo ao sistema de operação com vetores***\n"+
         "**************************************************\n"+
@@ -44,7 +44,7 @@ public class Projeto {
         "0. Sair\n");
 
             
-            do {
+            do {              /* o programa só inicia caso a função 1 seja escolhida */
                 System.out.println("**Por favor escolha a primeira função para assim poder utilizar as proximas**");
                 comando = leitor.nextInt();
             }while (comando!=1);
@@ -54,8 +54,8 @@ public class Projeto {
                 do{
 
                 n = leitor.nextInt();
-
-                if(n<0){
+                                              
+                if(n<0){             /* validação dos valores dentro da variável */
                     System.out.println("Porfavor, tome cuidado, não é possivel criar vetores negativos. Tente colocar um numero positivo");
                 }
 
@@ -71,13 +71,13 @@ public class Projeto {
 
                 m = leitor.nextInt();
 
-                if(m<0){
+                if(m<0){               /* validação dos números dentro variável */
                     System.out.println("O numero maximo tem que ser um numero inteiro positivo");
                 }
 
                 }while (m<0);
-
-            int[] vetor = new int[n];
+                                /* criação e preenchimento do vetor dado os valores que o usuario deu */
+            int[] vetor = new int[n];              
             for(int i = 0; i < vetor.length; i++){
 
             vetor[i] = random.nextInt(m);
@@ -102,7 +102,7 @@ public class Projeto {
 
         comando = leitor.nextInt();
 
-            switch (comando) {
+            switch (comando) {  /* chama as funções de acordo com a escolha do usuário e imprime no terminal */
                 case 1:
                     System.out.println("Você ja inicializou o vetor, tente fazer alguma operação.");
                 break;
@@ -164,8 +164,9 @@ public class Projeto {
 
 
 
-       public static void function2(int[]vetor){
-        System.out.println();
+       public static void function2(int[]vetor){  /* imprime os elementos de um vetor de inteiros */
+        System.out.println();                    /* Esta função itera sobre cada elemento do vetor de inteiros fornecido como parâmetro e 
+                                                  imprime cada elemento na mesma linha, separado por um espaço. */
         for(int i = 0; i < vetor.length; i++){
             System.out.print(vetor[i] + " ");
 
@@ -175,7 +176,7 @@ public class Projeto {
 
        public static int function3(int[]vetor,int num){
 
-        for(int i = 0; i < vetor.length; i++){
+        for(int i = 0; i < vetor.length; i++){              
             if(vetor[i] == num){
                 
                 return i;
@@ -186,11 +187,12 @@ public class Projeto {
            
        }
 
-    public static int function4(int[]vetor){
-
+    public static int function4(int[]vetor){ /*  verifica o maior valor dentro do vetor */
+                                                /* a função compara cada elemento dentro 
+                                                do vetor com o maior valor e ao final retorna o maior valor */     
         int maior = 0;
     
-    for(int i = 0; i < vetor.length; i++){
+    for(int i = 0; i < vetor.length; i++){      
         if(vetor[i] > maior){
 
             maior = vetor[i];
@@ -206,8 +208,8 @@ public class Projeto {
         int par = 0;
         int cont = 0;
 
-        for(int i = 0; i < vetor.length; i++){
-            if(vetor[i] % 2 == 0){
+        for(int i = 0; i < vetor.length; i++){ /* verifica os numeros pares, acumula na variavel par e faz uma média aritmética */
+            if(vetor[i] % 2 == 0){              /* ao fim retorna o valor da média dos números */
                 par += vetor[i];
                 cont++;
             } 
@@ -216,10 +218,10 @@ public class Projeto {
         
     }
 
-       public static int function6(int[] vetor){
-        int perc_imp = 0;
-
-        for(int i = 0; i<vetor.length;i++){
+       public static int function6(int[] vetor){ /* Calcula a porcentagem de números ímpares do vetor */
+        int perc_imp = 0;                        /* percorre cada elemento e verifica se é impar, */
+                                                  /* aumenta o contador a cada impar e calcula a porcentagem desses números */
+        for(int i = 0; i<vetor.length;i++){       /* em relação ao tamnho total do vetor*/
             if(vetor[i]%2 != 0){
             perc_imp++;
             }
@@ -227,33 +229,33 @@ public class Projeto {
          return perc_imp*100/vetor.length;
        }
 
-       public static int function7(int[] vetor){
-
-        int maiorN = 0;
-        int menorN = 9999;
-        int maiorNpos = 0;
-        int menorNpos = 0;
-        int media_centralizada = 0;
-
+       public static int function7(int[] vetor){ /* calcula uma média centralizada one o primeiro e ultimo valor */
+                                                  /* são desconsiderados */
+        int maiorN = 0;       /* guarda o maior número */
+        int menorN = 9999; /* guarda o menor número */
+        int maiorNpos = 0; /* guarda a posição do maior número */
+        int menorNpos = 0;  /* guarda a posição do menor número */
+        int media_centralizada = 0;   /* guarda o valor da média */
+                              
         for(int i = 0;i<vetor.length;i++){
-            if(vetor[i]>maiorN){
+            if(vetor[i]>maiorN){      /* percorre o vetor e verifica o maior número e sua posição */
                 maiorN = vetor[i];
                 maiorNpos = i;
             }
-            if(vetor[i]<menorN){
+            if(vetor[i]<menorN){ /* percorre o vetor e verifica o menor número e sua posição */
                 menorN = vetor[i];
                 menorNpos = i;
             }
         }
 
-        vetor[maiorNpos] = 0;
+        vetor[maiorNpos] = 0; /* atribui os valores a zero para não mudar o resultado do calculo*/
         vetor[menorNpos] = 0;
 
-        for(int j = 0;j<vetor.length;j++){
+        for(int j = 0;j<vetor.length;j++){    /* soma todos os elementos do vetor */
             media_centralizada += vetor[j];
         }
 
-        return media_centralizada/(vetor.length-2);
+        return media_centralizada/(vetor.length-2);/* Divide a soma pela quantidade de elementos do vetor e retorna o resulatdo */
     }
 
 
@@ -261,9 +263,9 @@ public class Projeto {
         boolean existe = false;
 
         for(int i = 0;i<vetor.length;i++){
-            for(int j = 0; j < vetor.length;j++){
-                if(vetor[i] + vetor[j] == valor && i!=j){
-                existe = true;
+            for(int j = 0; j < vetor.length;j++){         
+                if(vetor[i] + vetor[j] == valor && i!=j){  /* verifica se a soma dos dois valores resulta */
+                existe = true;               /* no valor inserido anteriormente e retorna caso verdadeiro ou falso */
                 }
             }
         }
